b5ce81520263d0b2b2e583401445ab01bd0b1291
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-01 17:37:17 +0900

    Title: 두 번의 연산, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240201/두 번의 연산/two-operations.java
+++ 240201/두 번의 연산/two-operations.java
@@ -6,12 +6,13 @@ public class Main {
 
         int a = sc.nextInt();
 
-        if(a%2!=0){
-            a +=3;
-            if(a%3==0){
-                a/=3;
-            }
+        if(a % 2 != 0){
+            a += 3;
         }
+        if(a % 3 == 0){
+            a /= 3;
+        }
+
         System.out.println(a);
     }
 }
