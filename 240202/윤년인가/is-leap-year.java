6da40826a80c704013681f98759d7fe8e3f4b1d7
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-02 17:06:09 +0900

    Title: 윤년인가, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/윤년인가/is-leap-year.java
+++ 240202/윤년인가/is-leap-year.java
@@ -5,13 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
         
-        if(y%4==0){
-            System.out.println("true");
-        } else if((y%4==0) && (y%100==0)){
-            System.out.println("false");
-        } else if(((y%4==0) && (y%100==0)) && (y%400 == 0)){
-            System.out.println("true");
-        } else{
+        if(y % 4 == 0){
+            if(y % 100 == 0){
+                if(y % 400 == 0){
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            } else {
+                System.out.println("true");
+            }
+        } else {
             System.out.println("false");
         }
     }
