901d849a8ee74f1275f33926e121b9e1cfc5d143
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-06 15:36:28 +0900

    Title: 숫자의 증감, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240206/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -11,12 +11,13 @@ public class Main {
             for(int i=1; i<=n; i++){
                 System.out.print(i + " ");
             }
+        
+        }
 
         if(c.equals("D")){
             for(int i=n; i>=1; i--){
                 System.out.print(i + " ");
             }
-        }
         }
     }
 }
