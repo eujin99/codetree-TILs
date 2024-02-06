073fe03daee26c5b2cec1cd7d73b1889ef8a0907
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-06 18:14:50 +0900

    Title: 369 게임, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/369 게임/369-game.java
+++ 240206/369 게임/369-game.java
@@ -4,21 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i<=n; i++){
+        
+        for(int i = 1; i <= n; i++) {
             boolean z = false;
-            if(i%10==3||i%10==6||i%10==9){
-                z = true;
+            int number = i;
+            
+           
+            while(number != 0) {
+                int digit = number % 10;
+                if(digit == 3 || digit == 6 || digit == 9) {
+                    z = true;
+                    break;
+                }
+                number /= 10;
             }
-            if(i%3==0){
+            
+            
+            if(i % 3 == 0)
                 z = true;
+            
+            if(z) {
+                System.out.print("0 ");
+            } else {
+                System.out.print(i + " ");
             }
-            if(z){
-            System.out.print("0" + " ");
         }
-        else{
-            System.out.print(i + " ");
-        }
-        }
-    
     }
 }
