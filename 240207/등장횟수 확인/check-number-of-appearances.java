4ff14816434bbd512b659491e1becb628288194a
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-07 22:21:02 +0900

    Title: 등장횟수 확인, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240207/등장횟수 확인/check-number-of-appearances.java
+++ 240207/등장횟수 확인/check-number-of-appearances.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
         int cnt = 0;
 
         for(int i=1;i<6;i++){
-            if(i%2==0){
+            int n = sc.nextInt();
+            if(n%2==0){
                 cnt++;
             }
         }
