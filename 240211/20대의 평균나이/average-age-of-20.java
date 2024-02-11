f90a0fc60f9fb3cbc13d04d7e01e961b3df246b4
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-11 12:21:21 +0900

    Title: 20대의 평균나이, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240211/20대의 평균나이/average-age-of-20.java
+++ 240211/20대의 평균나이/average-age-of-20.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int age;
         int sum = 0;
         int cnt = 0;
 
-        while (true) {
-            age = sc.nextInt();
+        while (sc.hasNextInt()) {
+            int age = sc.nextInt();
             if (age < 30) {
                 sum += age;
                 cnt++;
@@ -18,9 +17,13 @@ public class Main {
             }
         }
 
-        double avg = (double) sum / cnt;
-        System.out.printf("%.2f", avg);
-        
-        sc.close(); // while 루프 종료 후에 Scanner를 닫음
+        if (cnt > 0) { // 입력된 값이 적어도 하나 있을 때만 평균 계산
+            double avg = (double) sum / cnt;
+            System.out.printf("%.2f", avg);
+        } else {
+            System.out.println("No input provided."); // 입력이 없는 경우 메시지 출력
+        }
+
+        sc.close(); // Scanner 닫기
     }
 }
