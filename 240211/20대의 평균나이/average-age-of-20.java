a30db6aefc9b0aa90b7d2b9d6700ec8f8faf4c9d
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-11 22:34:10 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240211/20대의 평균나이/average-age-of-20.java
+++ 240211/20대의 평균나이/average-age-of-20.java
@@ -4,26 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int sum = 0;
-        int cnt = 0;
+        int age;
+        int sum=0;
+        int i = 0;
 
-        while (sc.hasNextInt()) {
-            int age = sc.nextInt();
-            if (age < 30) {
-                sum += age;
-                cnt++;
-            } else {
+        while(true){
+            age = sc.nextInt();
+            if(age<30){
+            i += age;
+            sum++;
+            }
+
+            if(age>=30){
                 break;
             }
         }
-
-        if (cnt > 0) { // 입력된 값이 적어도 하나 있을 때만 평균 계산
-            double avg = (double) sum / cnt;
-            System.out.printf("%.2f", avg);
-        } else {
-            System.out.println("No input provided."); // 입력이 없는 경우 메시지 출력
-        }
-
-        sc.close(); // Scanner 닫기
+        double avg=(double)i/sum;
+        System.out.printf("%.2f",avg);
     }
 }
