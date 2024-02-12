258966395806e170d8186ce771073a453693cfd7
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-12 13:38:12 +0900

    Title: 3n + 1 수열, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240212/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        while(true){
+            if(n%2==0){
+                n=n/2;
+                sum++;
+            }
+            else{
+                n=n*3+1;
+                sum++;
+            }
+            if(n==1){
+                break;
+            }
+        }
+        System.out.print(sum);
+    }
+}
