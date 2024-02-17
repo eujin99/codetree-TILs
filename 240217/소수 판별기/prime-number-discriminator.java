dccdd19110c74786d295e243600950f90786a458
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-17 23:59:15 +0900

    Title: 소수 판별기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240217/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        if (isPrime(n)) {
+            System.out.println("P");
+        } else {
+            System.out.println("C");
+        }
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
