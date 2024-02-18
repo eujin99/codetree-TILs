aa5be6114d5c9d727ee1fe5b3551a225fa243951
eujin99 <155924495+eujin99@users.noreply.github.com>
2024-02-18 21:13:35 +0900

    Title: 모두 3의 배수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240218/모두 3의 배수/all-multiples-of-3.java
+++ 240218/모두 3의 배수/all-multiples-of-3.java
@@ -4,17 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean ss= false;
+        boolean ss= true;
         int a;
 
         for(int i=1;i<=5;i++){
             a = sc.nextInt();
-            if(a%3==0){
-                ss=true;
+            if(a%3!=0){
+                ss=false;
+                break;
             }
         }
 
-        if(ss){
+        if(ss==true){
             System.out.println("1");
         }else{
             System.out.println("0");
